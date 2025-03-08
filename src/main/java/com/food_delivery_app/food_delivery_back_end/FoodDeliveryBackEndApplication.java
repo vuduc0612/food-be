4fee7e4a980583c411c6137f39a6d8ca9bd2e5e2
vuduc0612 <ducvu8628@gmail.com>
2025-03-09 00:20:14 +0700

    init project

---
+++ src/main/java/com/food_delivery_app/food_delivery_back_end/FoodDeliveryBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.food_delivery_app.food_delivery_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food_delivery_app/food_delivery_back_end/FoodDeliveryBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_delivery_app.food_delivery_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
